eeb99ff7b74022a93997cc93fdb9cf7e8cdd04df
Fredrik Bäckman <fredrik.backman12@gmail.com>
2025-03-02 22:54:53 +0100

    d2p2 attempt

--- d2/src/main.rs
+++ d2/src/main.rs
@@ -1,9 +1,15 @@
 use std::fs::read_to_string;
-
+#[derive(Debug)]
 struct Line {
     reports: Vec<i32>,
 }
-impl Line {}
+impl Line {
+    fn remove_index(&self, index: usize) -> Line {
+        let mut vec = self.reports.clone();
+        vec.remove(index);
+        Line { reports: vec }
+    }
+}
 
 fn main() {
     let lines = read_lines("input.txt");
@@ -16,7 +22,58 @@ fn main() {
         })
         .filter(|line| distances_safe(line))
         .count();
-    println!("safe count: {safe_count}")
+    println!("safe count p1: {safe_count}");
+
+    p2(lines);
+}
+
+fn p2(lines: Vec<Line>) -> i32 {
+    let count = lines
+        .iter()
+        .filter(|line| {
+            if check_line_safe(line) {
+                return true;
+            }
+            match find_unsafe_indices(line) {
+                Some(indices) => indices
+                    .iter()
+                    .map(|index| {
+                        let line = line.remove_index(*index);
+                        let line_safe = check_line_safe(&line);
+                        if line_safe {
+                            println!("line safe: {:?}", line);
+                            return true;
+                        };
+                        false
+                    })
+                    .any(|v| v),
+                None => false,
+            }
+        })
+        .count();
+    println!("p2 count: {count}");
+    1
+}
+
+fn find_unsafe_indices(line: &Line) -> Option<Vec<usize>> {
+    let vec: Vec<usize> = create_iterator(line)
+        .enumerate()
+        .filter(|(_i, (a, b))| (*a - *b).abs() > 3)
+        .map(|(i, _)| i)
+        .collect();
+    if !vec.is_empty() { Some(vec) } else { None }
+}
+
+fn check_line_safe(line: &Line) -> bool {
+    check_direction_safe(line) && distances_safe(line)
+}
+fn check_direction_safe(line: &Line) -> bool {
+    let direction = check_direction(line);
+    match direction {
+        "asc" => true,
+        "desc" => true,
+        &_ => false,
+    }
 }
 
 fn check_direction(line: &Line) -> &str {
